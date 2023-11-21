c717a2f38ff9c1cf298e5786f5821951e26deac5
thanhUni0708 <thanhtm78@akagames.io>
2023-11-22 01:30:55 +0700

    updated 22-11-2023

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.firebase:firebase-auth:22.2.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    //load image from URL
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/quanlysinhvien/template_admin_activity.java
+++ app/src/main/java/com/example/quanlysinhvien/template_admin_activity.java
@@ -3,7 +3,10 @@ package com.example.quanlysinhvien;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -15,11 +18,24 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quanlysinhvien.databinding.ActivityTemplateAdminBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.net.Uri;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import androidx.annotation.NonNull;
+import com.google.firebase.database.DatabaseError;
 
 public class template_admin_activity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityTemplateAdminBinding binding;
+    private ImageView imageAva;
+    private TextView nameAd;
+    private TextView mailAd;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +45,7 @@ public class template_admin_activity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarTemplateAdmin.toolbar);
-
+        initUI();
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -41,6 +57,8 @@ public class template_admin_activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_template_admin);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        showUserInfo();
     }
 
     @Override
@@ -55,5 +73,77 @@ public class template_admin_activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_template_admin);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    private void initUI(){
+        mNavigationView = binding.navView;
+        imageAva = mNavigationView.getHeaderView(0).findViewById(R.id.imageAva);
+        nameAd = mNavigationView.getHeaderView(0).findViewById(R.id.nameAdmin);
+        mailAd = mNavigationView.getHeaderView(0).findViewById(R.id.textViewMail);
+    }
+    private void showUserInfo(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user == null){
+            return;
+        }
+        String uid = user.getUid();
+
+        //String name = user.getDisplayName();
+
+
+        /*if(name == null){
+            nameAd.setVisibility(View.GONE);
+        }else{
+            nameAd.setVisibility(View.VISIBLE);
+            nameAd.setText(name);
+        }*/
+        // Thực hiện truy vấn để đọc thông tin người dùng từ Firebase Realtime Database
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("DBUser").child(uid);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Nếu thông tin người dùng tồn tại, lấy tên từ cơ sở dữ liệu
+                    String nameFromDatabase = dataSnapshot.child("name").getValue(String.class);
+
+                    if (nameFromDatabase != null) {
+                        // Hiển thị tên từ cơ sở dữ liệu
+                        nameAd.setVisibility(View.VISIBLE);
+                        nameAd.setText(nameFromDatabase);
+                    } else {
+                        // Nếu không có tên từ cơ sở dữ liệu, hiển thị tên từ FirebaseUser
+                        String nameFromUser = user.getDisplayName();
+                        if (nameFromUser != null) {
+                            nameAd.setVisibility(View.VISIBLE);
+                            nameAd.setText(nameFromUser);
+                        } else {
+                            // Nếu cả hai đều không có, ẩn TextView tên
+                            nameAd.setVisibility(View.GONE);
+                        }
+                    }
+                } else {
+                    // Nếu không có dữ liệu người dùng trong cơ sở dữ liệu, hiển thị tên từ FirebaseUser
+                    String nameFromUser = user.getDisplayName();
+                    if (nameFromUser != null) {
+                        nameAd.setVisibility(View.VISIBLE);
+                        nameAd.setText(nameFromUser);
+                    } else {
+                        // Nếu cả hai đều không có, ẩn TextView tên
+                        nameAd.setVisibility(View.GONE);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Xử lý lỗi khi đọc từ cơ sở dữ liệu
+            }
+        });
+
+        String email = user.getEmail();
+        Uri photoUrl = user.getPhotoUrl();
+        mailAd.setText(email);
+        Glide.with(this).load(photoUrl).error(R.drawable.ic_avatar_default).into(imageAva);
+
     }
 }
