01a4ccd20beecfde156b145210c702d9c8626fd7
thanhUni0708 <thanhtm78@akagames.io>
2023-11-25 00:26:30 +0700

    updated 25-11-2023 12h26p AM

---
+++ app/src/main/java/com/example/quanglong/adapter/ManagerAdapter.java
@@ -0,0 +1,122 @@
+package com.example.quanglong.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.view.LayoutInflater;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.content.Context;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import com.example.quanglong.model.User;
+import com.example.quanlysinhvien.R;
+import com.example.quanlysinhvien.ThemUser;
+import com.example.quanlysinhvien.suaUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ManagerAdapter extends ArrayAdapter<User> {
+    @NonNull
+    private Activity activity;
+    private int resource;
+    @NonNull
+    private List<User> object;
+    public ManagerAdapter(@NonNull Activity activity,int resource,@NonNull List<User> object){
+        super(activity,resource,object);
+        this.activity = activity;
+        this.resource = resource;
+        this.object = object;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //moi sv tra ve mot view va luu lai position
+        LayoutInflater inflater = this.activity.getLayoutInflater();
+        View view = inflater.inflate(this.resource,null);
+        //Khai bao cac textView
+        TextView txtName = view.findViewById(R.id.txtName);
+        TextView txtAge = view.findViewById(R.id.txtAge);
+        TextView txtPhone = view.findViewById(R.id.txtPhone);
+        TextView txtStatus = view.findViewById(R.id.txtStatus);
+        TextView txtRole = view.findViewById(R.id.txtRole);
+        //lay user dua len textView
+        User user = this.object.get(position);
+        txtName.setText(user.getName());
+        txtPhone.setText(String.valueOf(user.getPhone()));
+        txtAge.setText(String.valueOf(user.getAge()));
+        txtStatus.setText(user.getStatus());
+        txtRole.setText(user.getRole());
+
+        ImageView iconMenu = view.findViewById(R.id.ic_menu);
+        iconMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(activity,view);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                        if(menuItem.getItemId() == R.id.item_themUser){
+                            // Toast.makeText(activity,"Bạn đã nhấn nút Thêm",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(activity, ThemUser.class);
+                            activity.startActivity(intent);
+                        }
+                        else if(menuItem.getItemId() == R.id.item_sua_User){
+                            //Toast.makeText(activity,"Bạn đã nhấn nút Sửa: "+user.getName(),Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(activity, suaUser.class);
+                            intent.putExtra("USER",user);
+                            activity.startActivity(intent);
+                        }
+                        else if(menuItem.getItemId() == R.id.item_xoa_User){
+                            //Toast.makeText(activity,"Bạn đã nhấn nút Xoá: "+user.getName(),Toast.LENGTH_LONG).show();
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myRef = database.getReference("DBUser");
+                            myRef.child(user.getId()).removeValue(new DatabaseReference.CompletionListener() {
+                                @Override
+                                public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                                    Toast.makeText(activity,"Xoa thanh cong",Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                        return false;
+                    }
+                });
+                //truyen popup menu de show len
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+                //show icon
+                try {
+                    Field field = popupMenu.getClass().getDeclaredField("mPopUp");
+                    field.setAccessible(true);
+                    Object popUpMenuHelper = field.get(popupMenu);
+                    Class<?> cls = Class.forName("com.android.internal.view.menu.MenuPopupHelper");
+                    Method method = cls.getDeclaredMethod("setForceShowIcon",new Class[] {boolean.class});
+                    method.setAccessible(true);
+                    method.invoke(popUpMenuHelper,new Object[]{true});
+                }catch (Exception e)
+                {
+                    Log.d("MYTAG","onclick: "+ e.toString());
+                }
+                popupMenu.show();
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/com/example/quanlysinhvien/ui/home/manager_home.java
+++ app/src/main/java/com/example/quanlysinhvien/ui/home/manager_home.java
@@ -8,17 +8,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.quanglong.adapter.ManagerAdapter;
 import com.example.quanglong.adapter.userAdapter;
+import com.example.quanglong.model.User;
 import com.example.quanlysinhvien.R;
 import com.example.quanlysinhvien.databinding.FragmentManagerHomeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class manager_home extends Fragment {
+    private ListView listUser2;
+    private ArrayList<User> userArrayList;
+    private ManagerAdapter adapter2;
     private FragmentManagerHomeBinding binding;
 
 
@@ -31,7 +44,41 @@ public class manager_home extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentManagerHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        listUser2 = binding.listUser2;
+        userArrayList = new ArrayList<>();
+        GetData();
+        adapter2 = new ManagerAdapter(requireActivity(), R.layout.custom_listview_item_manager,userArrayList);
+        listUser2.setAdapter(adapter2);
         return root;
+    }
+    private void GetData(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("DBUser");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Xoa du lieu tren list view va cap nhap lai
+                adapter2.clear();
+
+                for(DataSnapshot data : dataSnapshot.getChildren()){
+                    //Convert data qua user
+                    User user = data.getValue(User.class);
+                    if(user!= null && "employee".equals(data.child("role").getValue(String.class))){
+                        //them user vao listview
+                        user.setId(data.getKey());
+                        adapter2.add(user);
+                        Log.d("MYTAG","onDataChange: " + user.getName() );
+                    }
+                }
+                Toast.makeText(requireContext(),"Load Data Success",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(requireContext(),"Load Data Failed",Toast.LENGTH_LONG).show();
+                Log.d("MYTAG","onCancelled: "+ databaseError.toString());
+            }
+        });
     }
     @Override
     public void onDestroyView() {
